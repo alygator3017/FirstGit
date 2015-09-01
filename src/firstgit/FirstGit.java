09939f07225407afd4762e8ca69c5b1b14786972
Alyson <Alyson@10.30.19.237>
2015-08-31 19:02:23 -0500

    added cars, even number, months and string builder.

---
+++ src/firstgit/FirstGit.java
@@ -0,0 +1,13 @@
+package firstgit;
+
+/**
+ *
+ * @author Alyson
+ */
+public class FirstGit {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
